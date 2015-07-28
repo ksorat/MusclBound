3d28d025a5b93495a334da7ce82d4beb72b85220
Kareem Sorathia <kareem.sorathia@gmail.com>
2015-07-28 13:55:08 -0400

    Added new set of input decks for testing level set method.

---
+++ leveltest.m
@@ -0,0 +1,48 @@
+
+%Various tests of the level set method
+
+clear; close all;
+Model.recon = 'ppm';
+Model.solver = 'hll';
+
+config = 'leveljet';
+
+%Generic data
+Pic.view = true;
+Pic.val = 'd';
+Pic.pg = true;
+
+
+switch lower(config)
+    case{'leveljet'}
+        Model.Tfin = 50.0;
+        Model.Bds = [-5 5 -5 5];
+        Model.Nvec = [1024 1024]/4;
+        
+        
+        Init.rho0 = 1.0;
+        Init.P0 = 1;
+        Init.DelP = 100; %Pressure ratio
+        Init.Min = 10; %Inward Mach #
+        
+        %Jet structure
+        Init.cent = 0.0;
+        Init.rad = 0.5;
+        Init.disc = 'true';
+        Init.problem = 'flow';
+        
+        %Boundary conditions
+        Model.bcs.ibx = 'injet'; Model.bcs.obx = 'outflow';
+        Model.bcs.iby = 'outflow'; Model.bcs.oby = 'outflow';
+        Cs = sqrt( (5/3)*Init.P0/Init.rho0 ); Init.vin = Init.Min*Cs;
+        
+    otherwise
+        disp('Unknown problem');
+end
+        
+%Let's roll!
+
+Model.Init = Init;
+Model.Pic = Pic;
+[Grid Gas] = runsim(Model);
+
